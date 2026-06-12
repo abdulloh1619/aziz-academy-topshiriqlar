5acf0b1e36ff4cff0a2e32784cbec8130a3d683d
abdulloh1619 <abdullohabdujamolov119@gmail.com>
2026-06-12 10:39:47 +0500

    Add AI task: Bank hisobvarag'i

---
+++ ai_topshiriqlar/Bank_hisobvaragi.py
@@ -0,0 +1,10 @@
+# Bank hisobvarag'i
+# Kurs: Dasturlash / IT
+# Mavzu: 1-mavzu: Python va dasturlashga kirish
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+hisob = float(input())
+miqdor = float(input())
+foiz = float(input())
+print(hisob + qoshimcha)
